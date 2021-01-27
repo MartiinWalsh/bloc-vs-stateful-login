b9108ac7286c874ad3d2d7bc6a115f7905d08eb9
m-codes <martinwalsh.dev@gmail.com>
2021-01-27 19:43:51 +0000

    Initial login screen

--- bloc_login/lib/src/screens/login_screen.dart
+++ bloc_login/lib/src/screens/login_screen.dart
@@ -2,6 +2,41 @@ import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration:
+          InputDecoration(hintText: 'email@example.com', labelText: 'Email'),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      child: Text('Login'),
+      color: Colors.blueAccent.shade200,
+    );
   }
 }
